ea9ab1e153cd3389d9def8a54aca2f5baa159438
hope <tasanbannu@gmail.com>
2023-04-25 13:17:52 -0400

    Fix stylelint errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.scss';
 // import Calculator2 from './components/Calculator2';
-import Calculator3 from './components/Calculator';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
-    <Calculator3 />
+    <Calculator />
   );
 }
 
