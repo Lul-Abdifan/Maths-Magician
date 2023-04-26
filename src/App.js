d806828dfd915eae8a8f6d9e2fe2912110b00e0b
hope <tasanbannu@gmail.com>
2023-04-26 12:29:08 -0400

    Remove commented code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.scss';
-// import Calculator2 from './components/Calculator2';
 import Calculator from './components/Calculator';
-import Quote from './components/Quote';
 
 function App() {
   return (
     <div>
       <Calculator />
-      <Quote />
     </div>
   );
 }
