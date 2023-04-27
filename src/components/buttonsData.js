e3583e8c6329502d0d0e63e376c18e6f96f9c62d
hope <tasanbannu@gmail.com>
2023-04-27 12:10:12 -0400

    Create Button component as a chil

--- src/components/buttonsData.js
+++ src/components/buttonsData.js
@@ -1,1 +1,22 @@
-
+const buttonsData = [
+  { label: 'AC', value: 'AC' },
+  { label: '+/-', value: '+/-' },
+  { label: '%', value: '%' },
+  { label: '÷', value: '÷', className: 'orange-tag' },
+  { label: '7', value: '7' },
+  { label: '8', value: '8' },
+  { label: '9', value: '9' },
+  { label: 'x', value: 'x', className: 'orange-tag' },
+  { label: '4', value: '4' },
+  { label: '5', value: '5' },
+  { label: '6', value: '6' },
+  { label: '-', value: '-', className: 'orange-tag' },
+  { label: '1', value: '1' },
+  { label: '2', value: '2' },
+  { label: '3', value: '3' },
+  { label: '+', value: '+', className: 'orange-tag' },
+  { label: '0', value: '0', className: 'zero' },
+  { label: '.', value: '.' },
+  { label: '=', value: '=', className: 'orange-tag' },
+];
+export default buttonsData;
