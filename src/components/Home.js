0327791459c9567ca0149be37205bd884bb5c709
Lul-Abdifan <tasanbannu@gmail.com>
2023-05-01 16:27:38 -0400

    Add Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,46 @@
-import React from "react";
-const Home=()=> {
-  return (
-    <div className="home-page">
-      <h1 className="home-title">Welcome to our Page!</h1>
+import React from 'react';
+import './Layout.scss';
 
-      <p className="funny-history">
-        Once upon a time, in a land far, far away, there was a brilliant but
-        eccentric mathematician named Dr. Calc. Dr. Calc was obsessed with
-        creating the perfect calculator, one that could perform the most basic
-        mathematical operation of all: addition. Dr. Calc believed that addition
-        was the cornerstone of all mathematics, and that a calculator that could
-        only add would be the most elegant and efficient tool for mathematicians
-        and scientists alike. So, Dr. Calc poured all of his time, energy, and
-        resources into creating the ultimate addition calculator. He
-        experimented with different materials, circuit designs, and algorithms
-        until he finally had a prototype that he was satisfied with. This
-        calculator was a thing of beauty: sleek and shiny, with big, colorful
-        buttons and a satisfyingly clicky keypad. But there was a catch: Dr.
-        Calc's addition calculator was incredibly expensive. In fact, it was so
-        expensive that only the wealthiest and most prestigious universities and
-        research institutions could afford to buy one.
-      </p>
-      <p>
-        {" "}
-        The calculator was marketed as a "luxury item" for the intellectual
-        elite, and it quickly became a status symbol for mathematicians and
-        scientists around the world. Despite its exorbitant price tag, Dr.
-        Calc's addition calculator became wildly popular. Mathematicians raved
-        about its simplicity and elegance, and the scientific community hailed
-        it as a groundbreaking invention. But there were also critics who mocked
-        the calculator's limited functionality, and some even accused Dr. Calc
-        of being a charlatan who had hoodwinked the academic world. But Dr. Calc
-        didn't care. He was too busy basking in the glow of his own genius and
-        counting his mountains of money. He spent his days lounging in his
-        luxurious mansion, surrounded by his collection of rare books and exotic
-        pets. And every now and then, he would chuckle to himself and think,
-        "Who needs subtraction, multiplication, and division when you have the
-        power of addition?"
-      </p>
-    </div>
-  );
-}
+const Home = () => (
+  <div className="home-page">
+    <h1 className="home-title">Welcome to our Page!</h1>
+
+    <p className="funny-history">
+      Once upon a time, in a land far, far away, there was a brilliant but
+      eccentric mathematician named Dr. Calc. Dr. Calc was obsessed with
+      creating the perfect calculator, one that could perform the most basic
+      mathematical operation of all: addition. Dr. Calc believed that addition
+      was the cornerstone of all mathematics, and that a calculator that could
+      only add would be the most elegant and efficient tool for mathematicians
+      and scientists alike. So, Dr. Calc poured all of his time, energy, and
+      resources into creating the ultimate addition calculator. He
+      experimented with different materials, circuit designs, and algorithms
+      until he finally had a prototype that he was satisfied with. This
+      calculator was a thing of beauty: sleek and shiny, with big, colorful
+      buttons and a satisfyingly clicky keypad. But there was a catch: Dr.
+      Calc&apos;s addition calculator was incredibly expensive. In fact, it was so
+      expensive that only the wealthiest and most prestigious universities and
+      research institutions could afford to buy one.
+    </p>
+    <p className="funny-history">
+      {' '}
+      The calculator was marketed as a &ldquo;luxury item&rdquo; for the intellectual
+      elite, and it quickly became a status symbol for mathematicians and
+      scientists around the world. Despite its exorbitant price tag, Dr.
+      Calc&rsquo;s addition calculator became wildly popular. Mathematicians raved
+      about its simplicity and elegance, and the scientific community hailed
+      it as a groundbreaking invention. But there were also critics who mocked
+      the calculator&rsquo;s limited functionality, and some even accused Dr. Calc
+      of being a charlatan who had hoodwinked the academic world. But Dr. Calc
+      didn&rsquo;t care. He was too busy basking in the glow of his own genius and
+      counting his mountains of money. He spent his days lounging in his
+      luxurious mansion, surrounded by his collection of rare books and exotic
+      pets. And every now and then, he would chuckle to himself and think,
+      &ldquo;Who needs subtraction, multiplication, and division when you have the
+      power of addition?&rdquo;
+    </p>
+
+  </div>
+);
 
 export default Home;
