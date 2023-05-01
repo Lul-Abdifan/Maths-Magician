5469a7ea758858d6c637d3222384ee5e9dd0d1b7
Lul-Abdifan <tasanbannu@gmail.com>
2023-05-01 16:05:50 -0400

    Add Routes for navigation lists

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
+import Layout from './components/Layout';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
+import Home from './components/Home';
 
 function App() {
   return (
     <div>
-      <Calculator />
-      <Quote />
+      <BrowserRouter>
+        <Layout />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="quote" element={<Quote />} />
+          <Route path="*" element={<div>Not Found 404</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
